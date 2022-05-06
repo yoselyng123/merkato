d8727afc866d6739e02ae46040db51ee01fbf22e
Yoselyn Morle <m.yoselyn@correo.unimet.edu.ve>
2022-05-05 22:48:07 -0400

    Button design

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./button.module.css";
 
-function Button({ tag, icon }) {
-  return <div className={styles.button}>Button</div>;
+function Button({ tag, Icon }) {
+  return (
+    <div className={styles.button}>
+      {Icon && (
+        <div className={styles.iconWrapper}>
+          <Icon
+            sx={{ ":hover": { fill: "#FFF" }, fill: "#000" }}
+            fontSize="inherit"
+          />
+        </div>
+      )}
+      <p>{tag}</p>
+    </div>
+  );
 }
 
 export default Button;
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,11 +2,28 @@ import React from "react";
 import Button from "../Button/Button";
 import styles from "./sideBar.module.css";
 
+/* Icons */
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PieChartIcon from "@mui/icons-material/PieChart";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ReceiptIcon from "@mui/icons-material/Receipt";
+import ChatIcon from "@mui/icons-material/Chat";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import SettingsIcon from "@mui/icons-material/Settings";
+
 function SideBar() {
   return (
     <div className={styles.sidebar}>
-      <p>Sidebar</p>
-      <Button tag="Dashboard" icon="" />
+      <p>MERKATO</p>
+      <div className={styles.options}>
+        <Button tag="Dashboard" Icon={DashboardIcon} />
+        <Button tag="Categories" Icon={PieChartIcon} />
+        <Button tag="Favourite" Icon={FavoriteIcon} />
+        <Button tag="Orders" Icon={ReceiptIcon} />
+        <Button tag="Messages" Icon={ChatIcon} />
+        <Button tag="Top Deals" Icon={LocalOfferIcon} />
+        <Button tag="Settings" Icon={SettingsIcon} />
+      </div>
     </div>
   );
 }
