6e5c754a48be2006eb8d9cc4a5173b54230f3097
Yoselyn Morle <m.yoselyn@correo.unimet.edu.ve>
2022-05-06 16:45:50 -0400

    Styled the Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "./navBar.module.css";
 
+import { Avatar } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
 function NavBar() {
-  return <div className={styles.navbar}>NavBar</div>;
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.leftSection}>
+        <SearchIcon sx={{ fill: "#28AF61", fontSize: 30 }} />
+        <input
+          className={styles.searchBar}
+          type="text"
+          placeholder="Busca un producto"
+        />
+      </div>
+      <div className={styles.rightSection}>
+        <div className={styles.notification}>
+          <NotificationsIcon sx={{ fill: "#95D5B1", fontSize: 30 }} />
+        </div>
+        <Avatar src="sadsa" />
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
--- src/components/TopProducts/TopProducts.jsx
+++ src/components/TopProducts/TopProducts.jsx
@@ -5,14 +5,51 @@ import styles from "./topproducts.module.css";
 function TopProducts() {
   return (
     <div className={styles.products}>
+      <div className={styles.top}>
         <p className={styles.title}>Top Products</p>
+        <a href="#" className={styles.more}>
+          View More
+        </a>
+      </div>
+      <div className={styles.cardsWrapper}>
         <div className={styles.cards}>
-            <Product tag="Fruits" price="20.10$" img="https://cdn-icons-png.flaticon.com/512/1625/1625099.png" />
-            <Product tag="Bread" price="20.10$" img="https://cdn-icons-png.flaticon.com/512/3348/3348075.png" />
-            <Product tag="Vegetable" price="20.10$" img="https://cdn-icons-png.flaticon.com/512/765/765544.png" />
-            <Product tag="Fish" price="20.10$" img="https://cdn-icons.flaticon.com/png/512/3005/premium/3005150.png?token=exp=1651817854~hmac=13bbbed1ddd0b538aa7c33f468a3c750" />
-            <Product tag="Meat" price="20.10$" img="https://cdn-icons-png.flaticon.com/512/2224/2224259.png" />
+          <Product
+            tag="Fruits"
+            price="20.10$"
+            img="https://cdn-icons-png.flaticon.com/512/1625/1625099.png"
+          />
+          <Product
+            tag="Bread"
+            price="20.10$"
+            img="https://cdn-icons-png.flaticon.com/512/3348/3348075.png"
+          />
+          <Product
+            tag="Vegetable"
+            price="20.10$"
+            img="https://cdn-icons-png.flaticon.com/512/765/765544.png"
+          />
+          <Product
+            tag="Fish"
+            price="20.10$"
+            img="https://cdn-icons.flaticon.com/png/512/3005/premium/3005150.png?token=exp=1651817854~hmac=13bbbed1ddd0b538aa7c33f468a3c750"
+          />
+          <Product
+            tag="Meat"
+            price="20.10$"
+            img="https://cdn-icons-png.flaticon.com/512/2224/2224259.png"
+          />
+          <Product
+            tag="Meat"
+            price="20.10$"
+            img="https://cdn-icons-png.flaticon.com/512/2224/2224259.png"
+          />
+          <Product
+            tag="Meat"
+            price="20.10$"
+            img="https://cdn-icons-png.flaticon.com/512/2224/2224259.png"
+          />
         </div>
+      </div>
     </div>
   );
 }
