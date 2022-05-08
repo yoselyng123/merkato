6741497c747ef55689239123bcf46a9a425070bd
Yoselyn Morle <m.yoselyn@correo.unimet.edu.ve>
2022-05-08 17:07:45 -0400

    Fixed visuals in navbar and product

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,19 +2,18 @@ import React from "react";
 import styles from "./navBar.module.css";
 
 import { Avatar } from "@mui/material";
-import NotificationsIcon from "@mui/icons-material/Notifications";
 import SvgIcon from "@mui/material/SvgIcon";
 
 function NavBar() {
   return (
     <div className={styles.navbar}>
       <div className={styles.leftSection}>
-        <SvgIcon className={styles.searchIcon}>
+        <SvgIcon className={styles.searchIcon} sx={{ fill: "#A0D9B9" }}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" />
           </svg>
         </SvgIcon>
-        {/* <SearchIcon sx={{ fill: "#28AF61", fontSize: 30 }} /> */}
+        {/* <SearchIcon  /> */}
         <input
           className={styles.searchBar}
           type="text"
@@ -23,21 +22,27 @@ function NavBar() {
       </div>
       <div className={styles.rightSection}>
         <div className={styles.envelope}>
-          <SvgIcon className={styles.envelopeIcon}>
+          <SvgIcon className={styles.envelopeIcon} sx={{ fill: "#A0D9B9" }}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
               <path d="M0 128C0 92.65 28.65 64 64 64H448C483.3 64 512 92.65 512 128V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V128zM48 128V150.1L220.5 291.7C241.1 308.7 270.9 308.7 291.5 291.7L464 150.1V127.1C464 119.2 456.8 111.1 448 111.1H64C55.16 111.1 48 119.2 48 127.1L48 128zM48 212.2V384C48 392.8 55.16 400 64 400H448C456.8 400 464 392.8 464 384V212.2L322 328.8C283.6 360.3 228.4 360.3 189.1 328.8L48 212.2z" />
             </svg>
           </SvgIcon>
         </div>
         <div className={styles.notification}>
-          <SvgIcon className={styles.notificationsIcon}>
+          <SvgIcon
+            className={styles.notificationsIcon}
+            sx={{ fill: "#A0D9B9" }}
+          >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path d="M256 32V49.88C328.5 61.39 384 124.2 384 200V233.4C384 278.8 399.5 322.9 427.8 358.4L442.7 377C448.5 384.2 449.6 394.1 445.6 402.4C441.6 410.7 433.2 416 424 416H24C14.77 416 6.365 410.7 2.369 402.4C-1.628 394.1-.504 384.2 5.26 377L20.17 358.4C48.54 322.9 64 278.8 64 233.4V200C64 124.2 119.5 61.39 192 49.88V32C192 14.33 206.3 0 224 0C241.7 0 256 14.33 256 32V32zM216 96C158.6 96 112 142.6 112 200V233.4C112 281.3 98.12 328 72.31 368H375.7C349.9 328 336 281.3 336 233.4V200C336 142.6 289.4 96 232 96H216zM288 448C288 464.1 281.3 481.3 269.3 493.3C257.3 505.3 240.1 512 224 512C207 512 190.7 505.3 178.7 493.3C166.7 481.3 160 464.1 160 448H288z" />
             </svg>
           </SvgIcon>
         </div>
         <Avatar src="sadsa" />
-        <SvgIcon className={styles.downIcon}>
+        <SvgIcon
+          className={styles.downIcon}
+          sx={{ fill: "#9893B7", fontSize: 12 }}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
             <path d="M224 416c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L224 338.8l169.4-169.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-192 192C240.4 412.9 232.2 416 224 416z" />
           </svg>
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./product.module.css";
 
+import SvgIcon from "@mui/material/SvgIcon";
+
 function Product({ tag, img, price }) {
+  const [selected, setSelected] = useState(false);
+
   return (
     <div className={styles.product}>
-        <div className={styles.favorite}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"><path d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"/></svg>
+      <div className={styles.favorite} onClick={() => setSelected(!selected)}>
+        {selected ? (
+          <SvgIcon className={styles.favoriteIcon}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z" />
+            </svg>
+          </SvgIcon>
+        ) : (
+          <SvgIcon className={styles.favoriteIcon} sx={{ fill: "red" }}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z" />
+            </svg>
+          </SvgIcon>
+        )}
+      </div>
+      <img src={img} alt="" />
+      <p className={styles.name}>{tag}</p>
+      <div className={styles.footer}>
+        <div className={styles.pricebox}>
+          <p className={styles.price}>{price}</p>
+          <p className={styles.unit}>/unidad</p>
         </div>
-        <img src={img} alt="" />
-        <p className={styles.name}>{tag}</p>
-        <div className={styles.footer}>
-            <div className={styles.pricebox}>
-                <p className={styles.price}>{price}</p>
-                <p className={styles.unit}>/unidad</p>
-            </div>
-            <div className={styles.agregarcarrito}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor"><path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/></svg>
-            </div>
+        <div className={styles.agregarcarrito}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            fill="currentColor"
+          >
+            <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+          </svg>
         </div>
+      </div>
     </div>
   );
 }
