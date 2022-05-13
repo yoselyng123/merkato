ffe4eaef1fadf7be9fab1af354da2f6368fc8460
jesus13santi <jesus13santi@gmail.com>
2022-05-13 15:09:34 -0400

    Arreglado ahora si react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
