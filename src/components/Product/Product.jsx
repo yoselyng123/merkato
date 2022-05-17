999af95986c9a28654c58bddcfe47223266986ac
Javier Jerez <61585930+JavierJerezAntonetti@users.noreply.github.com>
2022-05-17 16:23:30 -0400

    advanced product info design

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,26 +1,10 @@
 import React, { useState } from "react";
 import styles from "./product.module.css";
-import SvgIcon from "@mui/material/SvgIcon";
 import DetalleProducto from "../DetalleProducto/DetalleProducto";
+import AddButton from "../AddButton/AddButton";
 
 function Product({ data }) {
   
-  const [click, setClick] = useState(false);
-  const [quantity, setQuantity] = useState(0);
-
-  const handleClick = (type) => {
-    if (type === "minus") {
-      if (quantity === 1) {
-        setClick(!click);
-        setQuantity(0);
-      } else {
-        setQuantity(quantity - 1);
-      }
-    } else {
-      setQuantity(quantity + 1);
-    }
-  };
-
   const [info, setInfo] = useState(false);
   const [itemInfo, setItemInfo] = useState(
     {
@@ -32,32 +16,38 @@ function Product({ data }) {
     }
   );
 
-    const handleCardClick = (product, info) => {
-      setInfo(!info);
+    const handleCardClick = (product) => {
+      setInfo(true);
+      console.log("Info set to true");
       itemInfo.nombre = product.nombre;
       itemInfo.descripcion = product.descripcion;
       itemInfo.id = product.id;
       itemInfo.foto_producto = product.foto_producto;
       itemInfo.precio_unidad = product.precio_unitario;
       setItemInfo(itemInfo);
-
     }
 
+    const handleClose = () => {
+      setInfo(false);
+      console.log("Info set to false");
+    }
 
 
   return (
     <div className={styles.product}>
-      <div className={styles.productcontainer} onClick={(e) => handleCardClick(data, info)}>
-        {info === true &&
-          (<div className={styles.infobackground}>
-            <div className={styles.infocontainer}>
-                <DetalleProducto info_producto={itemInfo}/>
-            </div>
-          </div>)}
+
+      {info === true &&
+        (<div className={styles.infobackground}>
+          <div className={styles.infocontainer}>
+              <DetalleProducto info_producto={itemInfo} click={handleClose}/>
+          </div>
+        </div>)}
+
+      <div className={styles.productcontainer} onClick={(e) => handleCardClick(data)}>
 
         <div className={styles.imgContainer}>
           <img
-            src={data.foto_producto}
+            src={data.foto_producto[0]}
             alt=""
           />
         </div>
@@ -71,51 +61,7 @@ function Product({ data }) {
         </div>
       </div>
 
-      <div className={styles.addContainer}>
-        {click ? (
-          <div className={styles.addMoreContainer}>
-            <div
-              className={styles.addMoreIconContainer}
-              onClick={() => handleClick("minus")}
-            >
-              <SvgIcon>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                  <path d="M400 288h-352c-17.69 0-32-14.32-32-32.01s14.31-31.99 32-31.99h352c17.69 0 32 14.3 32 31.99S417.7 288 400 288z" />
-                </svg>
-              </SvgIcon>
-            </div>
-            <p className={styles.addText}>{quantity}</p>
-            <div
-              className={styles.addMoreIconContainer}
-              onClick={() => handleClick("plus")}
-            >
-              <SvgIcon>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                  <path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z" />
-                </svg>
-              </SvgIcon>
-            </div>
-          </div>
-        ) : (
-          <div
-            className={styles.addProductContainer}
-            onClick={() => {
-              setClick(!click);
-              handleClick("plus");
-            }}
-          >
-            <div className={styles.addMoreIconContainer}>
-              <SvgIcon>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                  <path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z" />
-                </svg>
-              </SvgIcon>
-            </div>
-
-            <p className={styles.addText}>Add</p>
-          </div>
-        )}
-      </div>
+      <AddButton />
 
     </div>
   );
