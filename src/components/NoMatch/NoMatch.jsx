ed7936df3f380e51a71c2fb1b1b64cc6143cd7ed
Yoselyn Morle <m.yoselyn@correo.unimet.edu.ve>
2022-05-19 13:38:11 -0400

    Update component NoMatch

--- src/components/NoMatch/NoMatch.jsx
+++ src/components/NoMatch/NoMatch.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styles from "./noMatch.module.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function NoMatch() {
+  let { name } = useParams();
+
   return (
     <div className={styles.noMatch}>
       <img
         src="https://cdn-icons-png.flaticon.com/512/7465/7465857.png"
         alt=""
       />
-      <p className={styles.title}>There were no search results.</p>
+      <p className={styles.title}>There were no search results for {name}</p>
       <p className={styles.subtitle}>
         Please check your spelling or use different keywords.
       </p>
