50948b23720361648013b261e74286eda7663739
Yoselyn Morle <m.yoselyn@correo.unimet.edu.ve>
2022-05-30 22:31:41 -0400

    Update REVISAR

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,11 +9,16 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
+import { useState } from "react";
+import RoleTypes from "../RoleTypes/RoleTypes";
 
 const auth = firebaseExports.auth;
 const firestore = firebaseExports.db;
 
 function Login({ click, isRegistrando, setIsRegistrando }) {
+  const [rol, setRol] = useState("");
+  const [userUid, setUserUid] = useState("");
+
   const submitHandler = (e) => {
     e.preventDefault();
     const email = e.target.elements.email.value;
@@ -48,7 +53,7 @@ function Login({ click, isRegistrando, setIsRegistrando }) {
     const docuRef = doc(firestore, `users/${infoUsuario.user.uid}`);
     const consulta = await getDoc(docuRef);
     if (!consulta.exists()) {
-      setDoc(docuRef, { email: email, rol: "cliente" });
+      setDoc(docuRef, { email: email, rol: rol });
       const infoDocu = consulta.data();
       return infoDocu;
     }
@@ -62,11 +67,17 @@ function Login({ click, isRegistrando, setIsRegistrando }) {
         // The signed-in user info.
         const user = result.user;
         // ...
-        if (result && user) {
+        const querySnapshot = getDoc(doc(firestore, "users", user.uid));
+        console.log(querySnapshot.exists);
+
+        if (!querySnapshot.exists) {
           setDoc(doc(firestore, "users", user.uid), {
             email: user.email,
-            rol: "cliente",
+            rol: "",
           });
+          setUserUid(user.uid);
+          setRol("");
+          setIsRegistrando(true);
         }
       })
       .catch((error) => {
@@ -82,75 +93,145 @@ function Login({ click, isRegistrando, setIsRegistrando }) {
       });
   };
 
+  const handleBack = () => {
+    setRol("");
+  };
   return (
     <div className={styles.login}>
-      <div className={styles.exitbutton} onClick={() => click()}>
-        <SvgIcon>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-            <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
-          </svg>
-        </SvgIcon>
-      </div>
-      <p className={styles.title}>{isRegistrando ? "Sign Up" : "Log in"}</p>
-      <p style={{ fontSize: "0.9rem" }}>
-        {isRegistrando ? "Enter your email and password to get started." : null}
-      </p>
-      <form onSubmit={submitHandler} className={styles.form}>
-        <input type="text" placeholder="email" id="email" />
-        <input type="text" placeholder="password" id="password" />
-        {isRegistrando ? (
-          <p style={{ fontSize: "0.8rem", textAlign: "center" }}>
-            By continuing, you agree to our Terms of Service
-          </p>
-        ) : (
-          <p>
-            Forgot password? <span className={styles.link}>Reset it</span>
-          </p>
-        )}
-
-        <input
-          className={styles.submit}
-          type="submit"
-          value={isRegistrando ? "Sign Up" : "Log in"}
-        />
-
-        <hr />
-
-        <div className={styles.otherLogInBtn} onClick={handleSignInWithGoogle}>
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/300/300221.png"
-            alt=""
-            className={styles.icon}
+      {rol === "" && isRegistrando ? (
+        <div className={styles.contentContainer}>
+          <div
+            className={styles.exitbutton}
+            style={{ marginBottom: "1rem" }}
+            onClick={() => click()}
+          >
+            <SvgIcon>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
+              </svg>
+            </SvgIcon>
+          </div>
+          <RoleTypes
+            newLoginGoogle={isRegistrando}
+            firestore={firestore}
+            userUid={userUid}
+            setRol={setRol}
           />
-          <p>Continue with Google</p>
-        </div>
 
-        <hr />
+          <hr />
 
-        <div>
-          {isRegistrando ? (
-            <div className={styles.bottom}>
-              <p>Already have an account</p>
-              <p
-                className={styles.link}
-                onClick={() => setIsRegistrando(!isRegistrando)}
-              >
-                Log in
-              </p>
-            </div>
-          ) : (
-            <div className={styles.bottom}>
-              <p>Don't have an account</p>
-              <p
-                className={styles.link}
-                onClick={() => setIsRegistrando(!isRegistrando)}
-              >
-                Sign Up
-              </p>
-            </div>
-          )}
+          <div>
+            {isRegistrando ? (
+              <div className={styles.bottom}>
+                <p>Already have an account</p>
+                <p
+                  className={styles.link}
+                  onClick={() => setIsRegistrando(!isRegistrando)}
+                >
+                  Log in
+                </p>
+              </div>
+            ) : (
+              <div className={styles.bottom}>
+                <p>Don't have an account</p>
+                <p
+                  className={styles.link}
+                  onClick={() => setIsRegistrando(!isRegistrando)}
+                >
+                  Sign Up
+                </p>
+              </div>
+            )}
+          </div>
         </div>
-      </form>
+      ) : (
+        <>
+          <div className={styles.topSec}>
+            {isRegistrando && (
+              <div className={styles.exitbutton} onClick={() => handleBack()}>
+                <SvgIcon>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z" />
+                  </svg>
+                </SvgIcon>
+              </div>
+            )}
+
+            <div className={styles.exitbutton} onClick={() => click()}>
+              <SvgIcon>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                  <path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z" />
+                </svg>
+              </SvgIcon>
+            </div>
+          </div>
+          <p className={styles.title}>{isRegistrando ? "Sign Up" : "Log in"}</p>
+          <p style={{ fontSize: "0.9rem" }}>
+            {isRegistrando
+              ? "Enter your email and password to get started."
+              : null}
+          </p>
+          <form onSubmit={submitHandler} className={styles.form}>
+            <input type="text" placeholder="email" id="email" />
+            <input type="text" placeholder="password" id="password" />
+            {isRegistrando ? (
+              <p style={{ fontSize: "0.8rem", textAlign: "center" }}>
+                By continuing, you agree to our Terms of Service
+              </p>
+            ) : (
+              <p>
+                Forgot password? <span className={styles.link}>Reset it</span>
+              </p>
+            )}
+
+            <input
+              className={styles.submit}
+              type="submit"
+              value={isRegistrando ? "Sign Up" : "Log in"}
+            />
+
+            <hr />
+
+            <div
+              className={styles.otherLogInBtn}
+              onClick={handleSignInWithGoogle}
+            >
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/300/300221.png"
+                alt=""
+                className={styles.icon}
+              />
+              <p>Continue with Google</p>
+            </div>
+
+            <hr />
+
+            <div>
+              {isRegistrando ? (
+                <div className={styles.bottom}>
+                  <p>Already have an account</p>
+                  <p
+                    className={styles.link}
+                    onClick={() => setIsRegistrando(!isRegistrando)}
+                  >
+                    Log in
+                  </p>
+                </div>
+              ) : (
+                <div className={styles.bottom}>
+                  <p>Don't have an account</p>
+                  <p
+                    className={styles.link}
+                    onClick={() => setIsRegistrando(!isRegistrando)}
+                  >
+                    Sign Up
+                  </p>
+                </div>
+              )}
+            </div>
+          </form>
+        </>
+      )}
     </div>
   );
 }
