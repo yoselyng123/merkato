1268fb1d295d05df1f63e9f3e4ca1e1ac8279ac3
Javier Jerez <61585930+JavierJerezAntonetti@users.noreply.github.com>
2022-06-06 15:15:10 -0400

    AgregarProducto finished fully functional

--- src/components/AgregarProducto/AgregarProducto.jsx
+++ src/components/AgregarProducto/AgregarProducto.jsx
@@ -5,25 +5,28 @@ import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
 
 const AgregarProducto = ({ setProductos, idComercio, categorias }) => {
 
-  const [imagen, setImagen] = useState(null);
   const nombreRef = useRef(null);
   const descripcionRef = useRef(null);
   const precioRef = useRef(0.1);
   const categoriaRef = useRef(null);
   const stockRef = useRef(1);
-  const imagenRef = useRef(null);
 
   const cleanFields = () => {
+
     nombreRef.current.value = "";
     descripcionRef.current.value = "";
     precioRef.current.value = 0.1;
     categoriaRef.current.value = "unselected";
     stockRef.current.value = 1;
-    imagenRef.current.value = "";
-    setImagen(null);
+    setImageList(imageInputList);
   }
 
   const addtoFirebase = async () => {
+
+    const imageURLList = [];
+    imageList.forEach(image => {
+      imageURLList.push(image.value);
+    })
 
     const addSnapshot = await addDoc(collection(firebaseExports.db, "producto"), {
       id_categoria: categoriaRef.current.value,
@@ -32,7 +35,7 @@ const AgregarProducto = ({ setProductos, idComercio, categorias }) => {
       nombre: nombreRef.current.value,
       precio_unitario: Number(precioRef.current.value),
       stock: Number(stockRef.current.value),
-      foto_producto: [imagen],
+      foto_producto: imageURLList,
       pasillo: 0 //HAY QUE ESPECIFICAR EL PASILLO
     });
 
@@ -55,17 +58,76 @@ const AgregarProducto = ({ setProductos, idComercio, categorias }) => {
 
   }
 
+  //AQUI ES PARA LOS INPUTS DE LAS IMAGENES
+
+  const imageInputList = [{
+    value: ""
+  }]
+
+  const [imageList, setImageList] = useState(imageInputList);
+
+  const addInput = () => {
+    setImageList(s => {
+      return [
+        ...s, 
+        {
+          value: ""
+        }
+      ]
+    });
+  };
+
+  const removeInput = index => {
+    const newImageList = [...imageList];
+    newImageList.splice(index, 1);
+    setImageList(newImageList);
+  }
+
+  const handleChange = e => {
+    e.preventDefault();
+
+    const index = e.target.id;
+    setImageList(s => {
+      const newArr = s.slice();
+      newArr[index].value = e.target.value;
+
+      return newArr;
+    });
+  };
+
+
   return (
 
     <div className={styles.inputsBox}>
       <picture className={styles.boxImg}>
-        {imagen ? (<img src={imagen} alt="unavailable" />) : (<h3>Insert a Link</h3>)}
+        {imageList[0].value !== "" ? (<img src={imageList[0].value} alt="unavailable" />) : (<h3>Insert a Link</h3>)}
       </picture>
       <label htmlFor="imagenProducto">Image Link: </label>
-      <input type="text" ref={imagenRef} onChange={(e) => setImagen(e.target.value)} id="imagenProducto" name="imagenProducto" />
+
+      {imageList.map((item, index) => (
+        <div className={styles.imageInputs}>
+          <div className={styles.imageInput}>
+            <input 
+              value={item.value}
+              id={index}
+              type="text"
+              onChange={handleChange} 
+              required 
+            />
+
+            {imageList.length !== 1 && <button onClick={() => removeInput(index)} className={styles.button}>-</button>}
+          </div>
+          <div className={styles.addButton}>
+            {imageList.length - 1 === index && <button onClick={addInput} className={styles.button}>+</button>}
+          </div>
+        </div>
+      ))}
+      
+
+      
 
       <label htmlFor="nombreProducto">Name: </label>
-      <input type="text" ref={nombreRef} id="nombreProducto" name="nombreProducto" />
+      <input type="text" ref={nombreRef} id="nombreProducto" name="nombreProducto" required />
 
       <label htmlFor="categoriaProducto">Category: </label>
       <div className={styles.selectMenu}>
@@ -78,13 +140,13 @@ const AgregarProducto = ({ setProductos, idComercio, categorias }) => {
       </div>
 
       <label htmlFor="stockProducto">Stock: </label>
-      <input type="number" defaultValue={1} min={1} ref={stockRef} id="stockProducto" name="stockProducto" />
+      <input type="number" defaultValue={1} min={1} ref={stockRef} id="stockProducto" name="stockProducto" required />
       
       <label htmlFor="precioProducto">Price $:</label>
-      <input type="number" defaultValue={0.1} min={0.1} step={0.1} ref={precioRef} id="precioProducto" name="precioProducto" />
+      <input type="number" defaultValue={0.1} min={0.1} step={0.1} ref={precioRef} id="precioProducto" name="precioProducto" required />
       
       <label htmlFor="descripcionProducto">Description: </label>
-      <input type="text" ref={descripcionRef} id="descripcionProducto" name="descripcionProducto" />
+      <input type="text" ref={descripcionRef} id="descripcionProducto" name="descripcionProducto" required />
 
       <div className={styles.inputButtons}>
         <button onClick={addtoFirebase} className={styles.btnAgregarProd}>Add Product</button>
