ebc0fbf3decf797e55a8af28e8aa9ab98d959eff
Yoselyn Morle <m.yoselyn@correo.unimet.edu.ve>
2022-06-20 18:05:36 -0400

    Toast Messages in Favorites

--- src/components/AddFavorites/AddFavorites.jsx
+++ src/components/AddFavorites/AddFavorites.jsx
@@ -5,19 +5,30 @@ import SvgIcon from "@mui/material/SvgIcon";
 import { UserContext } from "../../context/UserContext";
 import { useContext } from "react";
 
+import toast from "react-hot-toast";
+
 function AddFavorites({ data, eliminarProductoFavorito }) {
   const { agregarACarritoFavorito, user, eliminarProductoCarritoFavorito } =
     useContext(UserContext);
-  const handleClick = async (productdata) => {
+  const handleClick = async () => {
     //haces tu peo de agregar el beta a favoritos aca
 
     if (user.carritoFavorito.findIndex((i) => i.id === data.id) === -1) {
       agregarACarritoFavorito(data.id, data.id_comercio);
-      alert("Producto Agregado a favoritos");
+      toast.success("Se ha agregado el producto a favoritos!");
     } else {
       eliminarProductoFavorito(data.id);
       eliminarProductoCarritoFavorito(data.id);
-      alert("Producto Eliminado de favoritos");
+      toast(() => (
+        <span style={{ display: "flex", alignItems: "center", gap: "0.8rem" }}>
+          <SvgIcon style={{ fill: "#FFCC00", fontSize: "1.5rem" }}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z" />
+            </svg>
+          </SvgIcon>
+          Producto eliminado de favoritos!
+        </span>
+      ));
     }
   };
 
