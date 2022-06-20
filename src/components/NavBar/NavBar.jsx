da864dbd5d8d49716bd0bf8b453a2b315ddae776
Yoselyn Morle <m.yoselyn@correo.unimet.edu.ve>
2022-06-20 17:43:50 -0400

    Update Firebase Deploy + Icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -107,7 +107,11 @@ function NavBar() {
   const handleSearch = (event) => {
     event.preventDefault();
     if (search !== "") {
-      navigate(`../searchBy/${search}`, { replace: true });
+      if (window.location.pathname === "/") {
+        navigate(`../searchBy/${search}`, { replace: true });
+      } else {
+        navigate(`../searchBy/${search}`, { replace: true });
+      }
     } else {
       navigate("/", { replace: true });
       setSearch("");
@@ -140,27 +144,34 @@ function NavBar() {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        <ListItem disablePadding className={styles.drawerBtn}>
-          <ListItemButton onClick={() => handleSignInClick()}>
-            <ListItemText
-              disableTypography
-              primary="Sign up"
-              className={styles.textBtn}
-            />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton onClick={() => handleLoginClick()}>
-            <ListItemIcon>
-              <SvgIcon>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                  <path d="M416 32h-64c-17.67 0-32 14.33-32 32s14.33 32 32 32h64c17.67 0 32 14.33 32 32v256c0 17.67-14.33 32-32 32h-64c-17.67 0-32 14.33-32 32s14.33 32 32 32h64c53.02 0 96-42.98 96-96V128C512 74.98 469 32 416 32zM342.6 233.4l-128-128c-12.51-12.51-32.76-12.49-45.25 0c-12.5 12.5-12.5 32.75 0 45.25L242.8 224H32C14.31 224 0 238.3 0 256s14.31 32 32 32h210.8l-73.38 73.38c-12.5 12.5-12.5 32.75 0 45.25s32.75 12.5 45.25 0l128-128C355.1 266.1 355.1 245.9 342.6 233.4z" />
-                </svg>
-              </SvgIcon>
-            </ListItemIcon>
-            <ListItemText primary="Log In" />
-          </ListItemButton>
-        </ListItem>
+        {!user && (
+          <>
+            <ListItem disablePadding className={styles.drawerBtn}>
+              <ListItemButton onClick={() => handleSignInClick()}>
+                <ListItemText
+                  disableTypography
+                  primary="Sign up"
+                  className={styles.textBtn}
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton onClick={() => handleLoginClick()}>
+                <ListItemIcon>
+                  <SvgIcon>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                    >
+                      <path d="M416 32h-64c-17.67 0-32 14.33-32 32s14.33 32 32 32h64c17.67 0 32 14.33 32 32v256c0 17.67-14.33 32-32 32h-64c-17.67 0-32 14.33-32 32s14.33 32 32 32h64c53.02 0 96-42.98 96-96V128C512 74.98 469 32 416 32zM342.6 233.4l-128-128c-12.51-12.51-32.76-12.49-45.25 0c-12.5 12.5-12.5 32.75 0 45.25L242.8 224H32C14.31 224 0 238.3 0 256s14.31 32 32 32h210.8l-73.38 73.38c-12.5 12.5-12.5 32.75 0 45.25s32.75 12.5 45.25 0l128-128C355.1 266.1 355.1 245.9 342.6 233.4z" />
+                    </svg>
+                  </SvgIcon>
+                </ListItemIcon>
+                <ListItemText primary="Log In" />
+              </ListItemButton>
+            </ListItem>
+          </>
+        )}
       </List>
       <Divider />
       <List>
