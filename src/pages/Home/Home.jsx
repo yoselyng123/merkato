179ff0d3cf37a6021a3f519a87eb5c3e074ad944
Yoselyn Morle <m.yoselyn@correo.unimet.edu.ve>
2022-06-20 22:24:21 -0400

    Update Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,22 +5,23 @@ import CurrentDeals from "../../components/CurrentDeals/CurrentDeals";
 import ListProducts from "../../components/ListProducts/ListProducts";
 import styles from "./home.module.css";
 import firebaseExports from "../../utils/firebaseConfig";
-import { query, where, collection, getDocs, getDoc, doc } from "firebase/firestore";
+import {
+  query,
+  where,
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+} from "firebase/firestore";
 
-function Home({
-  setProductos,
-  productos,
-  categorias,
-  pasillos
-}) {
-
+function Home({ setProductos, productos, categorias, pasillos }) {
   const { idcomercio } = useParams();
-  const [nombreComercio, setNombreComercio] = useState(null)
-  const [fotoComercio, setFotoComercio] = useState(null)
+  const [nombreComercio, setNombreComercio] = useState(null);
+  const [fotoComercio, setFotoComercio] = useState(null);
 
   useEffect(() => {
-    const ProductosFromFirebase = [];
     const subscriber = async () => {
+      const ProductosFromFirebase = [];
       await getDocs(
         query(
           collection(firebaseExports.db, "producto"),
@@ -32,29 +33,35 @@ function Home({
         });
         setProductos(ProductosFromFirebase);
         console.log(ProductosFromFirebase);
-      })
+      });
 
-      await getDoc(
-        doc(firebaseExports.db, "comercio", idcomercio)
-      ).then((doc) => {
-        const dataComercio = doc.data();
-        setNombreComercio(dataComercio.nombre)
-        setFotoComercio(dataComercio.foto)
-      })
+      await getDoc(doc(firebaseExports.db, "comercio", idcomercio)).then(
+        (doc) => {
+          const dataComercio = doc.data();
+          setNombreComercio(dataComercio.nombre);
+          setFotoComercio(dataComercio.foto);
+        }
+      );
+    };
 
-    }
-    
-    return () => subscriber();
+    subscriber();
   }, [setProductos, idcomercio]);
-
 
   return (
     <div className={styles.home}>
       <div className={styles.comercio}>
-        <img className={styles.comercio_foto} src={fotoComercio} alt="foto comercio" />
+        <img
+          className={styles.comercio_foto}
+          src={fotoComercio}
+          alt="foto comercio"
+        />
         <h2 className={styles.comercio_nombre}>{nombreComercio}</h2>
       </div>
-      <Categories categorias={categorias} idcomercio={idcomercio} pasillos={pasillos} />
+      <Categories
+        categorias={categorias}
+        idcomercio={idcomercio}
+        pasillos={pasillos}
+      />
       <CurrentDeals />
       <ListProducts products={productos} idComercio={idcomercio} />
     </div>
